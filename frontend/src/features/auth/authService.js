4c9bb7490ce3c1ec8a1652fa0ae506a78c03bb1c
istekhar khan <istekharkhan46@gmail.com>
2023-07-17 04:12:35 +0530

    updated for deploy

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'https://goalsetter-app-bw8x.onrender.com/api/users/'
+const API_URL = '/api/users/'
 
 const register = async(userData)=>{
   const response = await axios.post(API_URL,userData);
