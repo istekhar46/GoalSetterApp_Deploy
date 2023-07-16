6d553f3c35374831775e13b1e4954b495f13ac75
istekhar khan <istekharkhan46@gmail.com>
2023-07-17 03:04:50 +0530

    updated for deploy

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,56 +1,59 @@
 import { useSelector, useDispatch } from "react-redux";
-import {  useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import GoalForm from "../component/GoalForm";
 import GoalItem from "../component/GoalItem";
-import {getGoals,reset} from '../features/goals/goalSlice'
+import { getGoals, reset } from "../features/goals/goalSlice";
 import Spinner from "../component/spinner";
 
-function dashboard() {
+function Dashboard() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const { user } = useSelector((state) => state.auth);
-  const {goals, isLoading, isError, message} = useSelector((state)=> state.goal)
+  const { goals, isLoading } = useSelector(
+    (state) => state.goal
+  );
 
   useEffect(() => {
-    if(isError){
-      console.log(message)
-    }
+   
     if (!user) {
       navigate("/login");
     }
 
-    dispatch(getGoals())
+    dispatch(getGoals());
 
-    return ()=>{
-      dispatch(reset())
-    }
-  }, [user, navigate,dispatch]);
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, dispatch]);
 
-  if(isLoading){
-    return <Spinner/>
+  if (isLoading) {
+    return <Spinner />;
   }
 
-  return (<>
-  <section className="heading">
-      <h1>Welcome {user && user.name}</h1>
-      <p>Goal Dashboard</p>
-  </section>
+  return (
+    <>
+      <section className="heading">
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goal Dashboard</p>
+      </section>
 
-  <GoalForm/>
-  
-  <section className="content">
-    {goals.length > 0 ? (
-      <div className="goals">
-        {goals.map((goal)=>
-         <GoalItem key={goal._id} goal={goal} />
+      <GoalForm />
+
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any Goals</h3>
         )}
-      </div>
-    ):(<h3>You have not set any Goals</h3>)}
-  </section>
-  
-  </>)
+      </section>
+    </>
+  );
 }
 
-export default dashboard;
+export default Dashboard;
